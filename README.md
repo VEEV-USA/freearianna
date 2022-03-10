7a6a2e8d096a41d88f5475ca4ca1a4cb58078e07
expressd3v <expressd3v@gmail.com>
2022-03-10 13:36:59 -0800

    add project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Free Ariamma
---
+++ src/pages/takeAction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TakeActionPage = () => {
+    return (
+        <>
+            TakeActionPage
+        </>
+    )
+}
+
+export default TakeActionPage
