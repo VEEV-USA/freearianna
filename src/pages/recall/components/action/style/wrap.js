c7c0b1764f0a58947a5f33783d5e60e473b5a010
indexdefi <dbaran+space@gmail.com>
2022-05-20 13:47:38 -0700

    recall page

--- src/pages/recall/components/action/style/wrap.js
+++ src/pages/recall/components/action/style/wrap.js
@@ -4,4 +4,9 @@ const TakeActionWrap = styled.div`
   padding: 62px 0 108px;
 `
 
+export const Wrap1 = styled.div`
+  padding: 32px 0;
+  background: linear-gradient(to top,#fff,#fff);
+`
+
 export default TakeActionWrap
