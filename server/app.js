40d8babaa9bb700c2d73e9017fe37e8dcee06516
webcyspot <spot.yc55@gmail.com>
2022-05-31 19:36:21 +0200

    Fix the Mongodb

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use('/api/users', users);
 //   .then(console.log('MongoDB connected'))
 //   .catch(err => console.log(err)); // Maybe return res code 500
 
-const url = `mongodb+srv://root:<password>@cluster0.lbdtqsd.mongodb.net/test
+const url = `mongodb+srv://root:root@cluster0.lbdtqsd.mongodb.net/test
 `;
 
 
