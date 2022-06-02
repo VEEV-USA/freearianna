b7a0bfa48fcb2fbb458f4bf2bfe69fbc88d19377
webcyspot <spot.yc55@gmail.com>
2022-06-02 20:38:59 +0200

    Small bug style bug fix

--- freearianna/src/pages/Profileview/components/hero2/index.js
+++ freearianna/src/pages/Profileview/components/hero2/index.js
@@ -6,20 +6,20 @@ import P1 from "../../../../components/paragraph";
 import address from "./address.json"
 const KidsHero = ({person}) => {
     console.log('person',person)
-   
     var result=address.filter(obj=> obj.abbreviation == person );
     console.log("result",result[0].name)
     return (
         <KidsHeroWrap>
             <Container>
-                
+                <SectionTitle>
+                 <br></br>
+                </SectionTitle>
                 <h4>{result[0].name} Recall</h4>
-                {/* <P1>
-                    Text
-                </P1> */}
+                <h1>
+                <br></br>
+                </h1>
             </Container>
         </KidsHeroWrap>
     );
 };
-
 export default KidsHero;
