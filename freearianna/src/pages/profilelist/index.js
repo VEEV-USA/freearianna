551c699b41bfd0c70fef307df1e155f15b2f848e
red62labs <david@red62.com>
2022-06-15 19:18:09 -0400

    update

--- freearianna/src/pages/profilelist/index.js
+++ freearianna/src/pages/profilelist/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 import HomeHero from "./components/hero";
 import HomeStory from "./components/story";
 import HomeCriminals from "./components/criminals";
@@ -7,27 +7,26 @@ import HomeMine from "./components/mine";
 import HomeShare from "./components/share";
 import HomeTable from "./components/table";
 import HomeLast from "./components/last";
-import TakeActionContent from '../profileview/components/action';
+import TakeActionContent from "../profileview/components/action";
 import TakeActionHero2 from "../profileview/components/hero2";
 import { useLocation } from "react-router-dom";
 
-
 const ProfileList = () => {
-    const location = useLocation();
-    const person = location.state;
-    return (
-        <Fragment>
-            <TakeActionHero2 person={person}/>
-            <TakeActionContent person={person}/>
-            {/* <HomeStory/>
+  const location = useLocation();
+  const person = location.state;
+  return (
+    <Fragment>
+      <TakeActionHero2 person={person} />
+      <TakeActionContent person={person} />
+      {/* <HomeStory/>
             <HomeCriminals/>
             <HomeExtortion/>
             <HomeMine/>
             <HomeShare/>
             <HomeTable/>
             <HomeLast/> */}
-        </Fragment>
-    );
+    </Fragment>
+  );
 };
 
 export default ProfileList;
