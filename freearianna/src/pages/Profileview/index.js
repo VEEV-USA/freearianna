e34b082bb91d5672ad70bcc2ebe900125747bf24
red62labs <david@red62.com>
2022-06-15 19:31:23 -0400

    update

--- freearianna/src/pages/Profileview/index.js
+++ freearianna/src/pages/Profileview/index.js
@@ -1,16 +1,17 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from "react";
 import TakeActionHero2 from "./components/hero2";
 import TakeActionContent from "./components/action";
-import { ConsoleSqlOutlined } from '@ant-design/icons';
+import { ConsoleSqlOutlined } from "@ant-design/icons";
 
-export const TakeActionPage = ({person}) => {
-    console.log('fsadfa',person)
-    return (
-        <Fragment>
-            <TakeActionHero2 person={person}/>
-            <TakeActionContent person={person}/>
-        </Fragment>
-    )
-}
+export const TakeActionPage = ({ person }) => {
+  console.log("fsadfa", person);
+  console.log("update");
+  return (
+    <Fragment>
+      <TakeActionHero2 person={person} />
+      <TakeActionContent person={person} />
+    </Fragment>
+  );
+};
 
-export default TakeActionPage 
+export default TakeActionPage;
