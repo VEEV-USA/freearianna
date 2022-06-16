3163fbb0563085aa06cdf622fdcae1771369f4f0
indexdefi <dbaran+space@gmail.com>
2022-06-16 14:54:29 -0700

    dashboard & recalls

--- freearianna/src/layouts/landing.layout.js
+++ freearianna/src/layouts/landing.layout.js
@@ -5,17 +5,19 @@ import LayoutHeader from "./components/header";
 import LayoutFooter from "./components/footer";
 import HomePage from "../pages/home";
 import AboutPage from "../pages/about";
+import AriannaPage from "../pages/arianna";
 import CreatePage from "../pages/create";
 import LoginPage from "../pages/login";
+import Dashboard from "../pages/dashboard";
+import DonatePage from "../pages/donate";
 import EventsPage from "../pages/events";
-import TakeActionPage from "../pages/takeAction";
 import KidsPage from "../pages/kids";
-import RecallnowPage from "../pages/recallnow";
 import RecallPage from "../pages/recall";
+import RecallsPage from "../pages/recalls";
+import RecallnowPage from "../pages/recallnow";
+import TakeActionPage from "../pages/takeAction";
 import { ArrowUpOutlined } from "@ant-design/icons";
 import CircleButton from "../components/button/circle";
-import DonatePage from "../pages/donate";
-import AriannaPage from "../pages/arianna";
 import ProfileEditor from "../pages/profileeditor";
 import Profilelist from "../pages/profilelist";
 
@@ -29,15 +31,17 @@ const LandingLayout = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/arianna" element={<AriannaPage />} />
+          <Route path="/donate" element={<DonatePage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/kids" element={<KidsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/recallnow/:id/:username" element={<RecallnowPage />} />
-          <Route path="/recall" element={<RecallPage />} />
           <Route path="/create" element={<CreatePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/recall" element={<RecallPage />} />
+          <Route path="/recalls" element={<RecallsPage />} />
+          <Route path="/recallnow/:id/:username" element={<RecallnowPage />} />
           <Route path="/take-action" element={<TakeActionPage />} />
-          <Route path="/donate" element={<DonatePage />} />
-          <Route path="/arianna" element={<AriannaPage />} />
           <Route path="/profileeditor" element={<ProfileEditor />} />
           <Route path="/profilelist/:address" element={<Profilelist />} />
         </Routes>
