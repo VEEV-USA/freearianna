bbe7cd02628c5db0935b18aa509f895788960a49
indexdefi <dbaran+space@gmail.com>
2022-06-28 09:53:14 -0700

    client

--- freearianna/src/components/paragraph/copyright.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Copyright = styled.p`
-  color: #555555;
-  font-weight: 500;
-  font-size: 14px;
-`
-
-export default Copyright
--- freearianna/src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  auth: "mSgwWQQDuT8iBd3RgrrTligR2SsctOSxaDkq5FpN9p3RQUKz80EuqUFlznKfpwRs",
-  base_url:
-    window.location.hostname === "localhost"
-      ? "http://localhost:8787"
-      : "https://mongo-realm-worker.arianna-api.workers.dev",
-};
-
-export default config;
